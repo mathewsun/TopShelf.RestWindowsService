3bb8a569e75c24a0691bda7d44331c7c84814f84
Matvei Dubrovin <mathew.dubrovin@gmail.com>
2021-02-10 11:09:24 +0300

    first commit

---
+++ RestController.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+using System;
+using System.Web.Http;
+
+namespace TopShelf.RestWindowsService
+{
+    public class RestController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Ping()
+        {
+            try
+            {
+                return Ok(DateTime.Now);
+            } 
+            catch(Exception exc)
+            {
+                return BadRequest(exc.Message);
+            }
+        }
+        [HttpGet]
+        public IHttpActionResult Mirror(string jsonString)
+        {
+            try
+            {
+                //If the json string is converted, 
+                //then the json string is already
+                //json and we shouldn't convert it again
+                JObject.Parse(jsonString);
+
+                return Ok(jsonString);
+            }
+            catch
+            {
+                try
+                {
+                    //if the string is in the wrong json format
+                    //we convert the json string and return it
+                    return Ok(jsonString);
+                }
+                catch (Exception exc)
+                {
+                    //if we caught some error, we return its text
+                    return BadRequest(exc.Message);
+                }
+            }
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,30 @@
+﻿using Owin;
+using System.Linq;
+using System.Web.Http;
+
+namespace TopShelf.RestWindowsService
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = ConfigureApi();
+            app.UseWebApi(config);
+        }
+
+        private HttpConfiguration ConfigureApi()
+        {
+            var config = new HttpConfiguration();
+
+            config.Routes.MapHttpRoute(
+                "DefaultApi",
+                "api/{controller}/{action}",
+                new { id = RouteParameter.Optional });
+
+            var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
+
+            return config;
+        }
+    }
+}
